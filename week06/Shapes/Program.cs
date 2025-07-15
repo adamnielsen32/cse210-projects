9cd627a3968afc4365b8af8215789fbca6cfd67d
adamnielsen32 <nie15046@byui.edu>
2025-07-15 13:05:16 -0600

    Week6 Shapes activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Red", 2);
+        shapes.Add(s1);
+        Circle c1 = new Circle("Blue", 3);
+        shapes.Add(c1);
+        Rectangle r1 = new Rectangle("Green", 4, 5);
+        shapes.Add(r1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+        }
     }
 }
